fd239956ec2ce7defc3ebc1272cdd8757fb7d21f
ultrako <viceneare87@gmail.com>
2023-05-27 15:05:38 -0600

    Finished Group class

---
+++ LevelXML.test/GroupTest.cs
@@ -0,0 +1,20 @@
+using Xunit;
+using System;
+
+namespace HappyWheels.test;
+public class GroupTest
+{
+    [Fact]
+    public void GroupWithShape()
+    {
+        Rectangle rect = new();
+        rect.Fixed = false;
+        Group group = new(rect);
+        string expected =
+        @"<g x=""0"" y=""0"" ox=""0"" oy=""0"" s=""f"" f=""f"" o=""100"" im=""f"" fr=""f"">
+  <sh t=""0"" p0=""0"" p1=""0"" p2=""300"" p3=""100"" p4=""0"" p5=""f"" p6=""f"" p7=""1"" p8=""4032711"" p9=""-1"" p10=""100"" p11=""1"" />
+</g>";
+        string actual = group.ToString();
+        Assert.Equal(expected, actual, ignoreWhiteSpaceDifferences:true, ignoreLineEndingDifferences:true);
+    }
+}
--- LevelXML/Group.cs
+++ LevelXML/Group.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Xml.Linq;
 namespace HappyWheels;
 // A group contains Shapes and non interactive Specials
-public abstract class Group : Entity, IList<Entity>
+public class Group : Entity, IList<Entity>
 {
 	// So there's no such thing as a "make new empty group" button in the editor,
 	// but it would be useful to have an empty group with the default properties
@@ -10,10 +10,18 @@ public abstract class Group : Entity, IList<Entity>
 	public static string EditorDefault =
 	@"<g x=""0"" y=""0"" r=""0"" ox=""0"" oy=""0"" s=""f"" f=""f"" o=""100"" im=""f"" fr=""f"" />";
 	private List<Entity> lst;
-	public void Add(Entity entity) { lst.Add(entity); }
+	public void Add(Entity entity) 
+	{
+		checkGroupableEntity(entity); 
+		lst.Add(entity); 
+	}
 	public bool Remove(Entity entity) { return lst.Remove(entity); }
 	public int IndexOf(Entity entity) { return lst.IndexOf(entity); }
-	public void Insert(int i, Entity entity) { lst.Insert(i, entity); }
+	public void Insert(int i, Entity entity) 
+	{
+		checkGroupableEntity(entity); 
+		lst.Insert(i, entity); 
+	}
 	public void RemoveAt(int i) { lst.RemoveAt(i); }
 	public void Clear() { lst.Clear(); }
 	public bool Contains(Entity entity) { return lst.Contains(entity); }
@@ -22,6 +30,123 @@ public abstract class Group : Entity, IList<Entity>
 	IEnumerator<Entity> IEnumerable<Entity>.GetEnumerator() { return lst.GetEnumerator(); }
 	IEnumerator IEnumerable.GetEnumerator() { return lst.GetEnumerator();}
 	public int Count => lst.Count;
+	// We need to make sure only non fixed shapes, or non interactive specials are grouped.
+	// This function will throw an exception if that isn't the case.
+	private void checkGroupableEntity(Entity entity)
+	{
+		if (entity is Shape shape)
+		{
+			if (shape.Fixed == true)
+			{
+				throw new Exception("Only non fixed shapes can go into groups.");
+			}
+		} else if (entity is Special special)
+		{
+			// if (entity is Character character)
+			if (entity is TextBox)
+			{} else
+			{
+				throw new Exception($"{nameof(entity)} are not allowed in groups.");
+			}
+		} else
+		{
+			throw new Exception($"{nameof(entity)} are not allowed in groups.");
+		}
+	}
+	public Entity this[int index] 
+	{ 
+		get { return lst[index]; } 
+		set 
+		{
+			checkGroupableEntity(value); 
+			lst[index] = value; 
+		} 
+	}
+	public override double? x
+	{
+		get { return GetDoubleOrNull("x"); }
+		set
+		{
+			// Having triggers at NaN locations is actually useful;
+			// they can still be pointed to by triggers and activate other triggers.
+			double val = value ?? double.NaN;
+			elt.SetAttributeValue("x", val);
+		}
+	}
+	public override double? y
+	{
+		get { return GetDoubleOrNull("y"); }
+		set
+		{
+			double val = value ?? double.NaN;
+			elt.SetAttributeValue("y", val);
+		}
+	}
+	public double? OriginX
+	{
+		get { return GetDoubleOrNull("ox");}
+		set
+		{
+			double val = value ?? double.NaN;
+			elt.SetAttributeValue("ox", val);
+		}
+	}
+	public double? OriginY
+	{
+		get { return GetDoubleOrNull("oy");}
+		set
+		{
+			double val = value ?? double.NaN;
+			elt.SetAttributeValue("oy", val);
+		}
+	}
+	public HWBool? Sleeping
+	{
+		get { return GetBoolOrNull("s");}
+		set
+		{
+			HWBool val = value ?? false;
+			elt.SetAttributeValue("s", val);
+		}
+	}
+	public HWBool? Foreground
+	{
+		get { return GetBoolOrNull("f");}
+		set
+		{
+			HWBool val = value ?? false;
+			elt.SetAttributeValue("f", val);
+		}
+	}
+	public double? Opacity
+	{
+		get { return GetDoubleOrNull("o"); }
+		set
+		{
+			// If the opacity isn't set, the import box sets it to 100
+			double val = value ?? 100;
+			// If the opacity is set to NaN, the import box sets it to 0
+			elt.SetAttributeValue("o", Math.Clamp(val, 0, 100));
+		}
+	}
+	public HWBool? Fixed
+	{
+		get { return GetBoolOrNull("im");}
+		set
+		{
+			HWBool val = value ?? false;
+			elt.SetAttributeValue("im", val);
+		}
+	}
+	public HWBool? FixedRotation
+	{
+		get { return GetBoolOrNull("fr");}
+		set
+		{
+			HWBool val = value ?? false;
+			elt.SetAttributeValue("fr", val);
+		}
+	}
 	internal override void PlaceInLevel(Func<Entity, int> mapper)
 	{
 		elt.RemoveNodes();
@@ -31,19 +156,28 @@ public abstract class Group : Entity, IList<Entity>
 			elt.Add(entity.elt);
 		}
 	}
-	public Entity this[int index] { get { return lst[index]; } set { lst[index] = value; } }
-	public Group(string xml) : this(StrToXElement(xml)) {}
-	internal Group(XElement? e, Func<XElement, Entity> ReverseMapper=default!) : 
-		this(content: (e ?? new XElement("empty")).Elements()
-			.Select(element => Entity.FromXElement(element, ReverseMapper))
-			.ToArray()) 
-	{}
-	protected Group(params Entity[]? content) : base ("g") 
+	private void setParams(XElement e)
 	{
-		if (content is not null) {
-			lst = new(content);
-		} else {
-			lst = new();
-		}
+		x = GetDoubleOrNull(e, "x");
+		y = GetDoubleOrNull(e, "y");
+		OriginX = GetDoubleOrNull(e, "ox");
+		OriginY = GetDoubleOrNull(e, "oy");
+		Sleeping = GetBoolOrNull(e, "s");
+		Foreground = GetBoolOrNull(e, "f");
+		Opacity = GetDoubleOrNull(e, "o");
+		Fixed = GetBoolOrNull(e, "im");
+		FixedRotation = GetBoolOrNull(e, "fr");
+	}
+	public Group(params Entity[] content) : this(EditorDefault, content) {}
+	public Group(string xml, params Entity[]? content) : this(StrToXElement(xml), content:content) {}
+	internal Group(XElement e, Func<XElement, Entity> ReverseMapper=default!, params Entity[]? content) :
+	base("g")
+	{
+		IEnumerable<Entity> groupedEntities = 
+			e.Elements()
+			.Select(element => Entity.FromXElement(element, ReverseMapper))
+			.Concat(content ?? new Entity[0]);
+		lst = new(groupedEntities);
+		setParams(e);
 	}
 }
