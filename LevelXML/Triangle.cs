74c9b4fc6818829cbe609efe142ec78340e84cc7
dabi5851 <dabi5851@colorado.edu>
2023-09-18 00:50:49 -0600

    Added Triangle Shape

---
+++ LevelXML/Triangle.cs
@@ -0,0 +1,51 @@
+using System.Xml.Linq;
+
+namespace HappyWheels;
+
+/// <summary>
+/// A triangle shape
+/// </summary>
+public class Triangle : Shape
+{
+	internal override uint Type => 2;
+	public static string EditorDefault =
+        @"<sh t=""2"" p0=""0"" p1=""0"" p2=""200"" p3=""200"" p4=""0"" p5=""t"" p6=""f"" p7=""1"" p8=""4032711"" p9=""-1"" p10=""100"" p11=""1""/>";
+
+	public string ToXML() { return ToXML(mapper: default!); }
+
+	public override double? Width
+	{
+		get { return GetDoubleOrNull("p2"); }
+		set
+		{
+			double val = value ?? 100;
+			if (double.IsNaN(val)) 
+			{
+                throw new LevelXMLException("This would make the triangle disappear!");
+			} 
+			Elt.SetAttributeValue("p2", Math.Clamp(val, 5, 5000));
+		}
+	}
+
+	public override double? Height
+	{
+		get { return GetDoubleOrNull("p3"); }
+		set
+		{
+			double val = value ?? 300;
+			if (double.IsNaN(val)) {
+				throw new LevelXMLException("This would make the triangle disappear!");
+			} 
+			Elt.SetAttributeValue("p3", Math.Clamp(val, 15, 5000));
+		}
+	}
+
+	public Triangle() : this(EditorDefault) {}
+
+	public Triangle(string xml) : this(StrToXElement(xml)) {}
+	
+	internal Triangle(XElement e) : base(e)
+	{
+		SetParams(e);
+	}
+}
