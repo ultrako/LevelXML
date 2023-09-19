962ff0121defcf2ea92f65c6aaef344d0afaf0a3
dabi5851 <dabi5851@colorado.edu>
2023-09-18 18:20:16 -0600

    Added landmine

---
+++ LevelXML/LandMine.cs
@@ -0,0 +1,44 @@
+using System.Xml.Linq;
+
+namespace HappyWheels;
+
+/// <summary>
+/// A landmine object.
+/// As a reminder, this special has no trigger actions.
+/// Pointing a trigger to it with no actions will cause
+/// the landmine to detonate when the trigger is activated.
+/// </summary>
+public class Landmine : Special
+{
+    internal override uint Type => 2;
+    public const string EditorDefault = 
+    @"<sp t=""2"" p0=""0"" p1=""0"" p2=""0"" />";
+
+    public double? Rotation
+	{
+		get { return GetDoubleOrNull("p2"); }
+		set 
+		{ 
+			double val = value ?? 0;
+			if (double.IsNaN(val)) 
+			{
+				throw new LevelXMLException("That would make the special disappear!");
+			}
+			SetDouble("p2", val); 
+		}
+	}
+
+    protected override void SetParams(XElement e)
+    {
+        base.SetParams(e);
+        Rotation = GetDoubleOrNull(e, "p2");
+    }
+
+    public Landmine(string xml=EditorDefault) : this(StrToXElement(xml)) {}
+
+    internal Landmine(XElement e) : base(e)
+    {
+        SetParams(e);
+    }
+}
+
