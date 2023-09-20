585bffa50fcbd8d1cb3a87d2ccd8e2422b06c904
ultrako <viceneare87@gmail.com>
2023-09-19 22:34:10 -0600

    Added SpringPlatform

--- LevelXML.test/LevelTest.cs
+++ LevelXML.test/LevelTest.cs
@@ -689,6 +689,18 @@ public class LevelTest
 	}
 
 	[Fact]
+	public void ParseLevelWithSpringPlatform()
+	{
+		Level level = new(@"<levelXML>
+    <info v=""1.95"" x=""211"" y=""185"" c=""1"" f=""f"" h=""f"" bg=""0"" bgc=""16777215"" e=""1""/>
+    <specials>
+        <sp t=""5"" p0=""0"" p1=""0"" p2=""0"" />
+    </specials>
+</levelXML>");
+		Assert.IsType<SpringPlatform>(level.Specials[0]);
+	}
+
+	[Fact]
 	public void ParseLevelWithText()
 	{
 		Level level = new(@"<levelXML>
